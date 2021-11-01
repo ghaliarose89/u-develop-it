f0be70a6a879750643334ae4af777ac8b93af9b4
unknown <ghaliarose89@gmail.com>
2021-11-01 13:31:03 -0500

    sql test

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use (express.urlencoded({extended: false}));
-app.use (express.json);
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use((req,res)=>{
-    res.status(404).end();
+const db = mysql.createConnection( {
+    host:'localhost',
+    user: 'root',
+    password:'Mysqlpassword',
+    database : 'election'
+},
+console.log('Connected to the election database.')
+);
+
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
 });
 
-
-
-
-
-
-
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
